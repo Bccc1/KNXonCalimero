133825eb089deea2ece60153244932a4e736217a
Helyanwe <sven.schilling@gmx.net>
2014-12-10 15:40:27 +0100

    new class

---
+++ app/src/main/java/com/calimero/knx/knxoncalimero/core/Profile.java
@@ -0,0 +1,26 @@
+package com.calimero.knx.knxoncalimero.core;
+
+/**
+ * Created by Sven Schilling on 10.12.2014.
+ */
+public class Profile {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
